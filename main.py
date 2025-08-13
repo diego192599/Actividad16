e6d68c71aff19f82c413ddebf72115f0aa4a93bb
diego192599 <di.cramire@udeo.edu.gt>
2025-08-13 10:54:04 -0600

    creacion de clase biblioteca y clase de gestion de libros

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Menu")
+class bibloteca():
+    def __init__(self,titulo,autor,year,codigo):
+        self.titulo=titulo
+        self.autor=autor
+        self.year=year
+        self.codigo=codigo
+
+    def mostrar_info(self):
+        return f"Titulo del libro: {self.titulo}- Autor del libro: {self.autor}- Año de publicacion: {self.year}- Codigo del libro: {self.codigo}"
+class gestionLibors():
+    def __init__(self):
+        self.listaLibros=[]
+
+    def agregarLibro(self):
+        try:
+            cantidad=int(input("Ingrese la cantidad de libros que seran registrados: "))
+            for i in range(cantidad):
+                print(f"Libro #{i+1}")
+                titulo=input("Ingrese el titulo del libro: ")
+                autor=input("Ingrese el nombre del autor del dicho libro: ")
+                year=int(input("Ingrese el año de publicacion del libro: "))
+                codigo=input("Ingrese el codigo del libro: ")
+
+
+
